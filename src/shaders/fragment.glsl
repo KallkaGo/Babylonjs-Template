6910e663eed5703fb706e0f1b8a62c41712ba1de
More <765830637@qq.com>
2025-03-21 16:28:27 +0800

    update

--- src/shaders/fragment.glsl
+++ src/shaders/fragment.glsl
@@ -8,8 +8,6 @@ void main() {
 
     vec4 diffuse = texture(uDiffuse, vUv);
 
-    // diffuse.rgb = vec3(1., 1., 0.);
-
     gl_FragColor = vec4(pow(diffuse.rgb, vec3(1. / 2.2)), 1.);
-    gl_FragColor = vec4(diffuse.rgb, 1.);
+
 }
